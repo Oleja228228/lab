3b27d4de2744a12230bb8818ce6b403ac94f2eff
Oleg Roslik <lolkeker.207@gmail.com>
2025-09-22 21:37:45 +0300

    second attempt

--- python/laba1/task1.py
+++
@@ -1,4 +0,0 @@
-lastname = input("Write your lastname: ").capitalize()
-firstname = input("Write your firstname: ").capitalize()
-surname = input("Write your surname: ").capitalize()
-print(lastname," ", firstname[0],".", surname[0], ".", sep="")
