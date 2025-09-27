c337f0c47e67fa3a8f5068b8765ab488037142fc
Oleg Roslik <lolkeker.207@gmail.com>
2025-09-27 09:39:47 +0300

    remake task1

--- python/laba1/task1.py
+++ python/laba1/task1.py
@@ -1,4 +1,5 @@
-lastname = input("Write your lastname: ").capitalize()
-firstname = input("Write your firstname: ").capitalize()
-surname = input("Write your surname: ").capitalize()
+lastname = input("Write your lastname: ").lower()
+lastname = lastname[0].upper() + lastname[1:]
+firstname = input("Write your firstname: ").upper()
+surname = input("Write your surname: ").upper()
 print(lastname," ", firstname[0],".", surname[0], ".", sep="")
