aab7404697036f6895319adde50b7d8b28047c87
Oleg Roslik <lolkeker.207@gmail.com>
2025-09-27 09:45:54 +0300

    remake task3

--- python/laba1/task3.py
+++ python/laba1/task3.py
@@ -1,7 +1,8 @@
 password = input("Write your password: ")
+
 if len(password) < 16:
     print("Your password is too short")
-elif (password.isdigit() == False and password.isalpha() == True) or (password.isdigit() == True and password.isalpha() == False):
+elif password.isalpha() or password.isdigit():
     print("Your password is weak")
 else:
     print("Your password is strong")
