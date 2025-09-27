2a4ee15f5ddb1df0ef54a859fab39473f9090e4c
Oleg Roslik <lolkeker.207@gmail.com>
2025-09-27 09:50:05 +0300

    remake task9

--- python/laba1/task9.py
+++ python/laba1/task9.py
@@ -1,8 +1,12 @@
-ip = input("Enter ip address: ")
+ip = input("Enter ip: ")
 ip_sep = ip.split(".")
-if len(ip) < 15 or len(ip_sep) > 15:
+
+if len(ip_sep) != 4 or len(ip) > 15:
     print("Is not correct")
-elif (int(ip_sep[0]) < 0 or int(ip_sep[0]) > 255) or (int(ip_sep[1]) < 0 or int(ip_sep[1]) > 255) or (int(ip_sep[2]) < 0 or int(ip_sep[2]) > 255) or (int(ip_sep[3]) < 0 or int(ip_sep[3]) > 255):
+elif (int(ip_sep[0]) < 0 or int(ip_sep[0]) > 255 or
+      int(ip_sep[1]) < 0 or int(ip_sep[1]) > 255 or
+      int(ip_sep[2]) < 0 or int(ip_sep[2]) > 255 or
+      int(ip_sep[3]) < 0 or int(ip_sep[3]) > 255):
     print("Is not correct")
 else:
     print("Is correct")
