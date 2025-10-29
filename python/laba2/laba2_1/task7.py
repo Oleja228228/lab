4e6f81687b56f63c7eecb6fbbec6fe8204b6e18e
Oleg Roslik <lolkeker.207@gmail.com>
2025-10-29 17:41:11 +0300

    find mistake in task7

--- python/laba2/laba2_1/task7.py
+++ python/laba2/laba2_1/task7.py
@@ -3,11 +3,13 @@ input_str = input("Write your string: ")
 count = 1
 result = ""
 
-for i in range(1,len(input_str)):
-    if input_str[i] == input_str[i-1]:
+for i in range(1, len(input_str)):
+    if input_str[i] == input_str[i - 1]:
         count += 1
     else:
-        result += input_str[i-1]+str(count)
+        result += input_str[i - 1] + str(count)
         count = 1
-print(result)
 
+result += input_str[-1] + str(count)
+
+print(result)
