cf991e8deede121502c28837ba2ecb048ab87a79
Oleja228228 <lolkeker.207@gmail.com>
2025-11-14 10:15:13 +0300

    Delete python/laba1/task1.py

--- python/laba1/task1.py
+++
@@ -1,5 +0,0 @@
-lastname = input("Write your lastname: ").lower()
-lastname = lastname[0].upper() + lastname[1:]
-firstname = input("Write your firstname: ").upper()
-surname = input("Write your surname: ").upper()
-print(lastname," ", firstname[0],".", surname[0], ".")
