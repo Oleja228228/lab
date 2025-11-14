007eb41297b406a6eb6e83e6fb99a03b0e655449
Oleja228228 <lolkeker.207@gmail.com>
2025-11-14 10:11:23 +0300

    Delete python/laba1/test.py

--- python/laba1/test.py
+++
@@ -1,9 +0,0 @@
-#Если длинна строки нечетное число и строка состоит только из цифр найти частно от деления на 7 если тоьлко из букви и нечетная заменить a на о в проотивном члучае вывести каждый второй элемент
-text = input("Write your text: ")
-length = len(text)
-if length % 2 != 0 and text.isdigit():
-    print(int(text) / 7)
-elif length % 2 != 0 and text.isdigit() == False:
-    print(text.replace("o", "a"))
-else:
-    print(text[1::2])
