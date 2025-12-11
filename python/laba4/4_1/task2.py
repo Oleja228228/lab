b436b4f7d330663ced0651191ade7082c95a9a0e
Oleg Roslik <lolkeker.207@gmail.com>
2025-12-11 16:35:39 +0300

    lines have been removed in places where the function is undefined

--- python/laba4/4_1/task2.py
+++ python/laba4/4_1/task2.py
@@ -2,13 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-10, 10, 1000)
-y = 5 / (x**2 - 9)
 
-y[np.abs(x - 3) < 0.001] = np.nan
-y[np.abs(x + 3) < 0.001] = np.nan
+mask1 = x < -3
+mask2 = (x > -3) & (x < 3)
+mask3 = x > 3
+
+def f(x):
+    return 5 / (x**2 - 9)
 
 plt.figure(figsize=(8, 5))
-plt.plot(x, y, color='orange')
+
+plt.plot(x[mask1], f(x[mask1]), color='orange')
+plt.plot(x[mask2], f(x[mask2]), color='orange')
+plt.plot(x[mask3], f(x[mask3]), color='orange')
+
 plt.title('График функции f(x) = 5 / (x² - 9)')
 plt.xlabel('x')
 plt.ylabel('f(x)')
